2ebe11d98aefebf568d0493234610fe363b9b7fe
“baieceo <55915577@qq.com>
2022-08-27 02:19:45 +0800

    chore: 修改README.md

--- config.js
+++ config.js
@@ -2,20 +2,20 @@ module.exports = {
     host: 'https://cl.8715x.xyz', // 域名，cl.8715x.xyz现免翻墙，如被封请挂梯子访问t66y.com
     // 注册相关
     register: {
-        name: 'baiexxx', // 注册账号，请提前试好
-        pwd: 'baiexxx123', // 密码，请提前试好
-        email: 'baiexxx@qq.com', // 邮箱
+        name: 'baiecto', // 注册账号，请提前试好
+        pwd: 'baiecto@888', // 密码，请提前试好
+        email: 'baiecto@163.com', // 邮箱
         interval: 2000  // 破解间隔
     },
     // 爬虫相关
     spider: {
-        email: 'baiexxx@qq.com', // 开通服务的邮箱
-        host: 'smtp.qq.com', // 邮箱服务器主机，如：smtp.qq.com
-        service: 'qq', // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
+        email: 'baiecto@163.com', // 开通服务的邮箱
+        host: 'smtp.163.com', // 邮箱服务器主机，如：smtp.qq.com
+        service: '163', // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
         port: 465, // SMTP 端口
         secureConnection: true,
         auth: {
-            pass: 'gasddafcrqufnwnkjjbasdfige' // smtp授权码
+            pass: 'EUBXQCUOKWUVEHGY' // smtp授权码
         }
     }
 }
